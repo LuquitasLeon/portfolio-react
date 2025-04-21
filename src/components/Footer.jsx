3cd73897c3cb04352f2fa957a915b312deb26664
Lucas Leon <luquitax78@gmail.com>
2025-04-20 23:26:03 -0300

    feat: Creación del componente estudios y softskils

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <footer>
       <a href="https://linkedin.com/in/tu_usuario">LinkedIn</a>
+      <br />
       <a href="https://github.com/tu_usuario">GitHub</a>
     </footer>
   );
